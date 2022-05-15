f0c72259d73e3d853d0387499274cd6a8ba812a2
Denovo <vo0v0000@naver.com>
2022-05-16 04:35:00 +0900

    start setting

---
+++ config/express.js
@@ -0,0 +1,19 @@
+import express from "express";
+import config from "./config.js";
+import cors from "cors";
+import compression from "compression";
+import methodOverride from "method-override";
+
+export default (app) => {
+    app.use(compression());
+
+    app.use(express.json());
+
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use(methodOverride());
+
+    app.use(cors());
+
+    app.use(config.api.prefix, routes());
+};
